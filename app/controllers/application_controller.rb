500111573c14729543e2eb4d185e857098808b8e
girits <cube@3g.ua>
2012-12-25 10:49:46 +0200

    debug variables inside helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
-	helper_method :current_user
+ # protect_from_forgery
+	helper_method :current_user #Это хелпер метод контроллера, в нашем случае предназначен для использования методе current_user в любов контроллере и любой вьюхе
   
-  private
+#  private
 
+def test
+  @asd = "test"
+end
   	def current_user
-    	@current_user ||= Account.find_by_salt(cookies[:salt]) if cookies[:salt]
+  	  test
+  	  return @current_user if defined?(@current_user)
+      @current_user = Account.find_by_salt(cookies[:salt])  if cookies[:salt]
   	end
 end
