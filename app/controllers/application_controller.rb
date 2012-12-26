cbbd783579a271e608cfdb1ce6d2e0223f774eb2
girits <cube@3g.ua>
2012-12-26 12:49:39 +0200

    create http authentication in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,9 @@
 class ApplicationController < ActionController::Base
- # protect_from_forgery
+http_basic_authenticate_with :name => "netbee", :password => "netbee"
+  protect_from_forgery
 	helper_method :current_user #Это хелпер метод контроллера, в нашем случае предназначен для использования методе current_user в любов контроллере и любой вьюхе
   
-#  private
-
-def test
-  @asd = "test"
-end
   	def current_user
-  	  test
-  	  return @current_user if defined?(@current_user)
-      @current_user = Account.find_by_salt(cookies[:salt])  if cookies[:salt]
+       Account.find_by_salt(cookies[:salt])  if cookies[:salt]
   	end
 end
