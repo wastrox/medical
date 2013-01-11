2b571c6bf32b3ebbbb0c9d410256e45d7546e258
girits <cube@3g.ua>
2013-01-11 10:10:13 +0200

    setting production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Medical::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true 
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -43,10 +43,10 @@ Medical::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+   config.action_controller.asset_host = "http://medical.mac"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  #config.assets.precompile += %w( *.css, *.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
