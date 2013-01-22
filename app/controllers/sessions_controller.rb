fe67561b4c5632d770a67f1c912fab9b2a146527
girits <cube@3g.ua>
2013-01-22 11:50:03 +0200

    Refactoring code in the sessions_controller, action create: 1) created two method: find_account_by_email1 and require_active 2) and added, before_filter, new method (only create)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,12 @@
 class SessionsController < ApplicationController
+  before_filter :find_account_by_email, :only =>  [:create, :require_active]
+  before_filter :require_active, :only => [:create]
   def new
   end
 
   def create
-    account = Account.find_by_email(params[:email])
-		if account && account.active == false
-			redirect_to :controller => :confirmation, :action => :index
-    elsif account && account.authenticate(params[:password])
-			cookies.permanent[:salt] = account.salt
+    if @account && @account.authenticate(params[:password])
+			cookies.permanent[:salt] = @account.salt
       redirect_to root_url, notice: "Logged in!"
     else
       flash.now.alert = "Invalid email or password"
@@ -19,4 +18,17 @@ class SessionsController < ApplicationController
     cookies.delete(:salt)
     redirect_to root_url, notice: "Logged out!"
   end
+  
+  protected
+  
+  def find_account_by_email
+    @account = Account.find_by_email(params[:email])
+  end
+  
+  def require_active
+    unless @account.active? 
+      redirect_to :controller => :confirmation, :action => :index 
+    end
+  end
+  
 end
