83316517f65928da7ea0d9b1c9a1a008c24ba890
girits <cube@3g.ua>
2013-02-26 17:05:59 +0200

    Create State Machine for Vacancy

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -6,7 +6,36 @@ class Vacancy < ActiveRecord::Base
 	define_index do
 		indexes name 
 		indexes city
-		#has city
 		set_property :delta => :delayed
 	end
+	
+	state_machine :state, :initial => :draft do
+    event :request do
+      transition :draft => :pending
+    end
+    
+    event :approve_published do
+      transition :pending => :published
+    end
+    
+    event :approve_hot do
+      transition [:pending, :published] => :hot
+    end
+    
+    event :approve_rejected do
+      transition :pending => :rejected
+    end
+    
+    event :defer do
+      transition [:published, :hot] => :deferred
+    end
+    
+    event :drafting do
+      transition [:pending, :published, :hot, :rejected, :deferred] => :draft
+    end
+    
+    event :edit do
+      transition [:published, :hot, :rejected, :deferred] => :pending
+    end
+  end
 end
