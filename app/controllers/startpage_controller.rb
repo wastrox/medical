80f1cb5ca5f1ff295ed3a81bbc420f4628ec8acd
girits <cube@3g.ua>
2013-03-21 11:25:44 +0200

    Add search-test vacancy for startpage

--- app/controllers/startpage_controller.rb
+++ app/controllers/startpage_controller.rb
@@ -2,6 +2,8 @@ class StartpageController < ApplicationController
  layout "startpage"
  skip_before_filter :require_login, :only => [:index]
 	def index
+	  search_params = params[:search].to_s + " " + params[:city].to_s 
+		@vacancies = Vacancy.search(search_params)
 	end
 	
 	def testIndex
