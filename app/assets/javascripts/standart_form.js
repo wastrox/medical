db6a2bc502dbd4e988993486f49da06aaa93bd56
girits <nicholauskas@gmail.com>
2013-04-05 18:23:43 +0300

    fix create resume

--- app/assets/javascripts/standart_form.js
+++ app/assets/javascripts/standart_form.js
@@ -1,1 +1,32 @@
 // => FIXME: Объединить в отом файле все скрипты корорые были написаны для standart_form
+$(document).on('nested:fieldAdded', function(event){
+		var field = event.field; 
+		var dateTillExperience = field.find('.date-till');
+		var checkboxExperience = field.find('.current_workplace');
+
+		$(checkboxExperience).click(function() {
+			if (this.checked) {				
+	        	$(dateTillExperience).animate({ opacity: "0", height: 'toggle' }, 700);
+	    	}
+	    	else{
+	        	$(dateTillExperience).animate({ opacity: "1", height: 'toggle' }, 1000);
+	    	}
+		});
+
+		var dateTillEducation = field.find('.dateTillEducation');
+		var checkboxEducation = field.find('.student_now');
+
+		$(checkboxEducation).click(function() {
+			if (this.checked) {				
+	        	$(dateTillEducation).animate({ opacity: "0", height: 'toggle' }, 700)
+	    	}
+	    	else{
+	        	//$(dateTillEducation).css('display', 'block');
+	        	$(dateTillEducation).stop().animate({ opacity: "1", height: 'toggle' }, 1000);
+	    	}
+		});
+});	
+
+$(document).on('nested:fieldRemoved', function(event){
+	
+});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,21 @@
+#encoding: utf-8
 module ApplicationHelper
+
   def active_tab(url) # This method is used in the layouts/_navbar_admin.html.erb for change class="active" in the navigation panel
     "active" if self.current_page?(url) # => <li class="active">
+  end
+
+  def month_helper
+  	m = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
+  	return m
+  end
+
+  def year_list_helper
+  	year_list = Array.new
+  	year = Time.now.year - 50
+  	year.upto(Time.now.year) do	|i|
+  		year_list << i
+  	end
+  	return year_list.reverse!
   end
 end
