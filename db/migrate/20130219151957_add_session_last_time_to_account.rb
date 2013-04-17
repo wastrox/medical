c2bf4f2de43f223e007ca2688cb391fa2f64b105
girits <nicholauskas@gmail.com>
2013-04-17 18:06:56 +0300

    Fix bug vacancies/_form contacts, and fix migration session_last_time_to_account;

--- db/migrate/20130219151957_add_session_last_time_to_account.rb
+++ db/migrate/20130219151957_add_session_last_time_to_account.rb
@@ -1,5 +1,5 @@
 class AddSessionLastTimeToAccount < ActiveRecord::Migration
   def change
-    add_column :accounts, :session_last_time, :datetime
+    add_column :accounts, :session_last_time, :datetime, :default => 0
   end
 end
