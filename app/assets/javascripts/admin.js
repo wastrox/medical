f520ab1f0fda8c7bc26a328e48824cd2d59d1abb
girits <nicholauskas@gmail.com>
2013-04-17 17:27:00 +0300

    Fix style admin/(resumes/edit, vacancy/edit, company/profile/edit);

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,3 +1,6 @@
 $(function () {
 	$('.table_companies_index tr:first th, .table_companies_vacancy_index tr:first th').css('border-top', 'none');
+
+	// admin/companies/vacancy/edit - Кнопка "Опубликовать" становится не активна если компания не прошла проверку модератором
+	$(".disable").val("Компания не проверена").attr("disabled", "disabled");
 });
--- app/helpers/admin/companies/vacancy_helper.rb
+++ app/helpers/admin/companies/vacancy_helper.rb
@@ -1,8 +1,8 @@
 module Admin::Companies::VacancyHelper
   def helpers_class(company)
     class_submit = case company.state
-      when "draft", "pending", "rejected" then "disable"
-      else "save_edit_button"
+      when "draft", "pending", "rejected" then "disable input-sub btn btn-warning"
+      else "input-sub btn btn-warning"
     end
     
     return class_submit
