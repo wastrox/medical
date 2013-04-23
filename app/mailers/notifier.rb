f30d31b93e6e9fee7281ce97d11146422a6b12e3
girits <nicholauskas@gmail.com>
2013-04-23 16:31:56 +0300

    New activate_instructions for notifier;

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,14 +1,14 @@
+# encoding: utf-8
 class Notifier < ActionMailer::Base
   default from: "nicholauskas@gmail.com"
 
 	def activation_instructions(account)
-		@account_activation_url = activate_account_url(account.token)
-    mail(:to => account.email, :subject => "Welcome to My Awesome Site")
+		@account_token = account.token
+    	mail(:to => account.email, :subject => "Добро пожаловать на medical.netbee.ua")
 	end
 	
 	def letter_from_moderator(account, text)
 	  @body_letter = text
-	  mail(:to => account.email, :subject => "Letter from moderator")
+	  mail(:to => account.email, :subject => "Письмо от модератора")
 	end
-
 end
