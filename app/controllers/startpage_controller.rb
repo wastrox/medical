f97ab1ff69a53c2ea558b9c8dcdf84d07ac5e591
girits <nicholauskas@gmail.com>
2013-04-24 15:52:20 +0300

    Fix show last vacancies limit => 9 in last_page

--- app/controllers/startpage_controller.rb
+++ app/controllers/startpage_controller.rb
@@ -6,6 +6,6 @@ class StartpageController < ApplicationController
 		#search_params = params[:search].to_s + " " + params[:city].to_s 
 		#@vacancies = Vacancy.search(search_params)
 
-		@vacancies = Vacancy.where(:state => "published").order("created_at desc").limit(10)
+		@vacancies = Vacancy.where(:state => "published").order("created_at desc").limit(9)
 	end
 end
