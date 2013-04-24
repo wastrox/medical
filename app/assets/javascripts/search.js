882920837504e9781da80d76f61b148b0bb20544
girits <nicholauskas@gmail.com>
2013-04-24 17:05:19 +0300

    Fix show search/vacancy

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 	//Убирает логотип и аватар если они пусты
+	$("img").attr("alt", "Логотип");
+	
 	var img = $("img.photo-applicant");
 	if (img.attr("alt") == "Missing") {
     	img.parent().remove();
 	}
-
-	$("img").attr("alt", "Логотип");
 });
