741bc2b9b027608a71f727a011e5233118cafe82
girits <nicholauskas@gmail.com>
2013-04-24 11:27:34 +0300

    Fix resume show page for search - logo script;

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
+	//Убирает логотип и аватар если они пусты
+	var img = $("img.photo-applicant");
+	if (img.attr("alt") == "Missing") {
+    	img.parent().remove();
+	}
+
 	$("img").attr("alt", "Логотип");
 });
