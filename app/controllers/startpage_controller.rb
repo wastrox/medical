c4f9f27d9fa18d6330ef4d5e5fa6c89058c9f105
girits <nicholauskas@gmail.com>
2013-04-24 15:19:25 +0300

    Fix start page in last-vacancies

--- app/controllers/startpage_controller.rb
+++ app/controllers/startpage_controller.rb
@@ -1,8 +1,11 @@
 class StartpageController < ApplicationController
  layout "startpage"
  skip_before_filter :require_login, :only => [:index]
+
 	def index
-	  search_params = params[:search].to_s + " " + params[:city].to_s 
-		@vacancies = Vacancy.search(search_params)
+		#search_params = params[:search].to_s + " " + params[:city].to_s 
+		#@vacancies = Vacancy.search(search_params)
+
+		@vacancies = Vacancy.where(:state => "published").order("created_at desc").limit(10)
 	end
 end
