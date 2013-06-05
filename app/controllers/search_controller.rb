6df7cc894b1a0a86bc286d421c5930d29fa9fe86
Girits <nicholauskas@gmail.com>
2013-06-05 15:53:17 +0300

    Fix title

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,28 +7,28 @@ class SearchController < ApplicationController
 		search_params = params[:search].to_s + " " + params[:city].to_s 
 		if params[:sample] == "1"
 			@vacancies = Vacancy.search(search_params, :order => 'created_at desc')
-			@title = "Поиск вакансий #{search_params}"
+			@title = "Поиск вакансий #{search_params}: работа в медицине. Сайт трудоустройства medical.netbee.ua"
 		else
 			@resumes = Resume.search(search_params, :order => 'created_at DESC')
-			@title = "Поиск резюме #{search_params}"
+			@title = "Поиск резюме #{search_params}: работа в медицине. Сайт трудоустройства medical.netbee.ua"
 		end
 	end
 
 	def resume
 		@resume = Resume.find(params[:id])
 		@fullName = "#{@resume.profile.lastname} #{@resume.profile.firstname} #{@resume.profile.surename}"
-		@title = "Резюме #{@resume.position}"
+		@title = "Резюме #{@resume.position}: работа в медицине. Сайт трудоустройства medical.netbee.ua"
 
 	end
 
 	def vacancy
 		@vacancy = Vacancy.find(params[:id])
-		@title = "Вакансия #{@vacancy.name}"
+		@title = "Вакансия #{@vacancy.name}: работа в медицине. Сайт трудоустройства medical.netbee.ua"
 	end
 
 	def company
 		@company = Company.find(params[:id])
 		@vacancies = @company.vacancies.where(:state => ["published", "hot"])
-		@title = "Компания #{@company.name}"
+		@title = "Компания #{@company.name}: работа в медицине. Сайт трудоустройства medical.netbee.ua"
 	end
 end
