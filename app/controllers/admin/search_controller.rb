20e4afc734901f5b3500a9ff51153c713aba5760
Girits <nicholauskas@gmail.com>
2013-06-18 16:04:02 +0300

    Fix bug del account for admin

--- app/controllers/admin/search_controller.rb
+++ app/controllers/admin/search_controller.rb
@@ -21,16 +21,18 @@ skip_before_filter :require_login
 
 	def destroy_account_respond
 		account = Account.find(params[:id])
+		
 		if account.employer?
 		   resume_respond = ResumeRespond.find_by_employer_id(account.id).destroy unless resume_respond.nil?
-		   account.company.destroy unless account.company.nil? && account.destroy
+		   account.company.destroy unless account.company.nil? 
 		elsif account.applicant?
 			vacancy_respond = VacancyRespond.find_by_applicant_id(account.id).destroy unless vacancy_respond.nil?
-			account.resume.destroy unless account.resume.nil? && account.destroy
-		else
-			account.destroy
+			account.resume.destroy unless account.resume.nil? 
 		end
-        redirect_to admin_search_url
+
+		if account.destroy
+        	redirect_to admin_search_url
+        end
 	end
 	
 end
