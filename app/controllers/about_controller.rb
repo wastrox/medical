c04457005854f17272d70b0b05e7d399dde573e1
girits <nicholauskas@gmail.com>
2013-08-14 16:27:12 +0300

    Fix last vacancy for about page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -5,7 +5,7 @@ skip_before_filter :require_login, :only => [:index]
 
 
   def index
-	@vacancies = Vacancy.where(:state => "published").order("created_at desc").limit(3)
+	@vacancies = Vacancy.where(:state => "published").order("created_at desc").limit(7)
 	@title = "О нас: работа в медицине. Сайт трудоустройства medical.netbee.ua"
 	@description = "О компании Netbee. Самый большой выбор работы в медицине. Сайт трудоустройства medical.netbee.ua."
   end
--- app/controllers/personal_data_controller.rb
+++ app/controllers/personal_data_controller.rb
@@ -4,7 +4,7 @@ class PersonalDataController < ApplicationController
   skip_before_filter :require_login, :only => [:index]
 
   def index
-	@vacancies = Vacancy.where(:state => "published").order("created_at desc").limit(3)
+	@vacancies = Vacancy.where(:state => "published").order("created_at desc").limit(7)
 	@title = "Пользовательское соглашение: работа в медицине. Сайт трудоустройства medical.netbee.ua"
 	@description = "Пользовательское соглашение компании Netbee. Самый большой выбор работы в медицине. Сайт трудоустройства medical.netbee.ua."
   end
