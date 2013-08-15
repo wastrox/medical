d5bdc16b5bb727504886dc8062ac583b7b73ccfe
girits <nicholauskas@gmail.com>
2013-08-15 13:26:21 +0300

    Fix companies

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@
 
 class SearchController < ApplicationController
 	layout "search"
-	skip_before_filter :require_login, :only => [:index, :resume, :vacancy, :company, :scope, :category]
+	skip_before_filter :require_login, :only => [:index, :resume, :vacancy, :company, :scope, :category, :all_company]
 	
 	def index
 		search_params = params[:search].to_s + " " + params[:city].to_s 
