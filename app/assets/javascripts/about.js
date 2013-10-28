c55f89e3a6deb2db63b368ef8f06933511be700a
girits <inreworking@gmail.com>
2013-10-28 17:11:00 +0200

    Add script in articles

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
 	$(".boxed-nav .line-bottom:last-child").remove();
 
-	$("#frame a[href^='http://']").attr("target","_blank");
+	$("#frame a[href^='http://']").attr("target","_blank").attr("rel","nofollow");
 });
