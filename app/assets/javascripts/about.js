3478086795a980314f5fbbf29061160c4bbe2394
Kolya Girits <nicholauskas@gmail.com>
2014-02-26 17:29:31 +0200

    Fix news

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -2,4 +2,5 @@ $(document).ready(function(){
 	$(".boxed-nav .line-bottom:last-child").remove();
 
 	$("#frame a[href^='http://']").attr("target","_blank").attr("rel","nofollow");
+	$(".news_url").attr("target","_self");
 });
